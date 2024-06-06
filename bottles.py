3a72d78664d69f27e9f1477e3a473297c850e0ec
ERIC_BASKOVICH <eric.baskovich@gmail.com>
2024-06-06 11:56:47 -0600

    solution

--- bottles.py
+++ bottles.py
@@ -1,7 +1,9 @@
-def bottle_song():
-	# write your code here!
-	pass
+def bottle_song(num):
+	
+	while (num > 1):
+		lyric = f"{num} bottles of beer on the wall, {num} bottles of beer. Take one down and pass it around, {num - 1} bottles of beer on the wall."
+		print(lyric)
+		num -= 1
 
-bottle_song()
+bottle_song(6)
 
-####kldjsfalkfdajalskdfjdfls
